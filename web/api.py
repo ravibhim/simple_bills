0e3bd4934556e162666f9b82e6ac4f3349018aaf
Ravi Bhim <ravi.bhim@yahoo.com>
2016-10-18 15:08:11 -0700

    API endpoints WIP

--- web/api.py
+++ web/api.py
@@ -3,7 +3,10 @@ from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
 
-from api_models import StringMessage
+from google.appengine.ext.ndb import Key
+
+from api_models import *
+from models import *
 from utils import *
 
 EMAIL_SCOPE = endpoints.EMAIL_SCOPE
@@ -16,7 +19,14 @@ API_EXPLORER_CLIENT_ID = endpoints.API_EXPLORER_CLIENT_ID
                 scopes=[EMAIL_SCOPE])
 class SimpleBillApi(remote.Service):
 
-    @endpoints.method(message_types.VoidMessage, StringMessage,
+    def _copyAccountIdsMessage(self, profile):
+        aim = AccountIdsMessage()
+        for accountId in profile.accountIds:
+            aim.data.append(accountId)
+        aim.check_initialized()
+        return aim
+
+    @endpoints.method(message_types.VoidMessage, AccountIdsMessage,
             path='getAccountIdsCreated',
             http_method='POST', name='getAccountIdsCreated')
     def getAccountIdsCreated(self, request):
@@ -25,8 +35,42 @@ class SimpleBillApi(remote.Service):
             raise endpoints.UnauthorizedException('Authorization required')
 
         profile = getProfile(user)
+        return self._copyAccountIdsMessage(profile)
 
-        return StringMessage(data='Here is the list {}'.format(map(str,profile.accountIds).join(profile.accountIds)))
+
+    def _copyAccountBills(self, accountId):
+        blm = BillListMessage()
+
+        account_key = Key(Account, accountId)
+        account = account_key.get()
+        bills = Bill.query(ancestor=account_key).order(-Bill.date)
+        for bill in bills:
+            bm = BillMessage()
+            bm.billId = bill.key.id()
+            bm.amount = bill.amount
+
+            bm.day = bill.day
+            bm.month = bill.month
+            bm.year = bill.year
+            bm.check_initialized()
+
+            blm.bills.append(bm)
+        blm.check_initialized()
+        return blm
+
+    @endpoints.method(BillListFormMessage, BillListMessage,
+            path='getBillsList',
+            http_method='POST', name='getBillsList')
+    def getBills(self, request):
+        user = endpoints.get_current_user()
+        if not user:
+            raise endpoints.UnauthorizedException('Authorization required')
+
+        profile = getProfile(user)
+        if request.accountId in profile.accountIds:
+            return self._copyAccountBills(request.accountId)
+        else:
+            raise endpoints.ForbiddenException('Current user not authorized to access account_id {}'.format(request.accountId))
 
 
 
--- web/api_models.py
+++ web/api_models.py
@@ -3,3 +3,23 @@ from protorpc import messages
 class StringMessage(messages.Message):
     """StringMessage-- outbound (single) string message"""
     data = messages.StringField(1, required=True)
+
+class AccountIdsMessage(messages.Message):
+    data = messages.IntegerField(1, repeated=True)
+
+class BillListFormMessage(messages.Message):
+    accountId = messages.IntegerField(1, required=True)
+
+class BillMessage(messages.Message):
+    billId = messages.IntegerField(1, required=True)
+    amount = messages.IntegerField(2)
+
+    day = messages.IntegerField(3)
+    month = messages.IntegerField(4)
+    year = messages.IntegerField(5)
+
+class BillListMessage(messages.Message):
+    bills = messages.MessageField(BillMessage,1,repeated=True)
+
+
+
