7de30f7d4323fd01269af85233d28413b4db445b
Pradeep G <itsprdp@gmail.com>
2017-01-17 15:10:48 +0530

    Show file icons in the web search view https://github.com/ravibhim/simple_bills/issues/76

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,7 +1,7 @@
 var simpleBills = angular.module("SimpleBills", []);
 
 simpleBills.config(function($interpolateProvider) {
-    $interpolateProvider.startSymbol('#{').endSymbol('}');
+  $interpolateProvider.startSymbol('#{').endSymbol('}');
 });
 
 simpleBills.controller("AddAccountController", function($scope) {
@@ -12,173 +12,195 @@ simpleBills.controller("AddAccountController", function($scope) {
 });
 
 simpleBills.controller("SearchBillController", function($scope) {
-    $scope.accountId = PageConfig ? PageConfig.accountId : "";
-    $scope.accountTags = PageConfig ? PageConfig.accountTags : [];
-    $scope.stats = PageConfig ? PageConfig.stats : {};
-    $scope.bills = [];
-    $scope.search_tags = {};
+  $scope.accountId = PageConfig ? PageConfig.accountId : "";
+  $scope.accountTags = PageConfig ? PageConfig.accountTags : [];
+  $scope.stats = PageConfig ? PageConfig.stats : {};
+  $scope.bills = [];
+  $scope.search_tags = {};
 
-    var format_search_tags = function() {
-        var tags = "";
+  var format_search_tags = function() {
+    var tags = "";
 
-        for (var tag in $scope.search_tags) {
-            if ($scope.search_tags[tag] === true) {
-                tags += "search_tags=" + tag + "&";
-            }
-        }
+    for (var tag in $scope.search_tags) {
+      if ($scope.search_tags[tag] === true) {
+        tags += "search_tags=" + tag + "&";
+      }
+    }
 
-        return tags.slice(0, -1);
-    };
+    return tags.slice(0, -1);
+  };
 
-    $scope.accountTags.forEach(function(tag) {
-        $scope.search_tags[tag] = false;
+  $scope.accountTags.forEach(function(tag) {
+    $scope.search_tags[tag] = false;
+  });
+
+  $scope.fetchBills = function() {
+    // Show the spinner
+    $('#spinner-container').show();
+    $.ajax({
+      method: 'GET',
+      url: "/account/" + $scope.accountId + "/search_bills?" + format_search_tags(),
+      data: {
+        search_query: $scope.search_query,
+        search_start_date: $scope.search_start_date,
+        search_end_date: $scope.search_end_date
+      },
+      success: function(data) {
+        $scope.bills = data.results;
+        $scope.$digest();
+        $('#spinner-container').hide();
+      },
+      error: function() {
+        location.reload();
+      }
+    });
+  };
+
+  $scope.imageThumbnail = function(file) {
+    var thumbUrl = "/static/images/file256.png";
+
+    switch(file.file_type) {
+      case "application/pdf":
+        thumbUrl = "/static/images/pdf256.png";
+        break;
+      case "image/png":
+        thumbUrl = file.thumbnail;
+        break;
+      case "image/jpg":
+        thumbnail = file.thumbnail;
+        break;
+      case "image/jpeg":
+        thumbnail = file.thumbnail;
+        break;
+
+    }
+
+    return thumbUrl;
+  };
+
+  $scope.searchOnEnterKey = function($event) {
+    if ($event.keyCode == 13) {
+      $scope.fetchBills();
+    }
+  };
+
+  $scope.isTagSelected = function(tag) {
+    return $scope.search_tags[tag];
+  };
+
+  var monthNames = moment.months();
+  $scope.currentYear = moment().year();
+  $scope.currentMonth = moment().month();
+  $scope.currentMonthName = monthNames[$scope.currentMonth];
+
+  $scope.setStartEndDates = function() {
+    var startDay = moment([$scope.currentYear, $scope.currentMonth]).startOf('month').date();
+    var endDay = moment([$scope.currentYear, $scope.currentMonth]).endOf('month').date();
+
+    $scope.search_start_date = [$scope.currentYear, ($scope.currentMonth + 1), startDay].join('-');
+    $scope.search_end_date = [$scope.currentYear, ($scope.currentMonth + 1), endDay].join('-');
+  };
+
+  $scope.setStartEndDates();
+
+  $scope.selectedYearMonthsData = function() {
+    return $scope.allYearsStats()[$scope.currentYear];
+  };
+
+  $scope.updateCurrentYearAndMonthsData = function() {
+    $scope.setStartEndDates();
+    $scope.monthsData = $scope.selectedYearMonthsData();
+    $scope.determineTheMonthToShow();
+    $scope.fetchBills();
+  };
+
+  $scope.determineTheMonthToShow = function() {
+    // select the months that has bills
+    var data = _.filter($scope.monthsData, function(m) { return m.billCount > 0; });
+
+    if (data.length > 0) {
+      var preSelectedMonth;
+      // If the selected year is the current year then pre-select the current month
+      if ($scope.currentYear == moment().year()) {
+        preSelectedMonth = moment().month();
+      } else {
+        // Pre-select the month based on the available data for that particular year
+        preSelectedMonth = moment().month(data[0].monthName).month();
+      }
+
+      $scope.updateCurrentMonth(preSelectedMonth, false);
+    }
+  };
+
+  $scope.updateCurrentMonth = function(month, disabled) {
+    if(disabled) {} else {
+      $scope.currentMonth = month;
+      $scope.setStartEndDates();
+      $scope.currentMonthName = monthNames[month];
+      $scope.fetchBills();
+    }
+  };
+
+  $scope.allYearsStats = function() {
+    var data = {};
+
+    _.each($scope.getStatsYears(), function(year) {
+      data[year] = [];
+      _.each(moment.monthsShort(), function(m) { data[year].push({monthName: m, billCount: 0}); });
     });
 
-    $scope.fetchBills = function() {
-        // Show the spinner
-        $('#spinner-container').show();
-        $.ajax({
-            method: 'GET',
-            url: "/account/" + $scope.accountId + "/search_bills?" + format_search_tags(),
-            data: {
-                search_query: $scope.search_query,
-                search_start_date: $scope.search_start_date,
-                search_end_date: $scope.search_end_date
-            },
-            success: function(data) {
-                $scope.bills = data.results;
-                $scope.$digest();
-                $('#spinner-container').hide();
-            },
-            error: function() {
-                location.reload();
-            }
-        });
-    };
+    _.each($scope.stats.year_stats, function(yearStat) {
+      var year = parseInt(yearStat.year);
 
-    $scope.searchOnEnterKey = function($event) {
-      if ($event.keyCode == 13) {
-        $scope.fetchBills();
-      }
-    };
-
-    $scope.isTagSelected = function(tag) {
-        return $scope.search_tags[tag];
-    };
-
-    var monthNames = moment.months();
-    $scope.currentYear = moment().year();
-    $scope.currentMonth = moment().month();
-    $scope.currentMonthName = monthNames[$scope.currentMonth];
-
-    $scope.setStartEndDates = function() {
-      var startDay = moment([$scope.currentYear, $scope.currentMonth]).startOf('month').date();
-      var endDay = moment([$scope.currentYear, $scope.currentMonth]).endOf('month').date();
-
-      $scope.search_start_date = [$scope.currentYear, ($scope.currentMonth + 1), startDay].join('-');
-      $scope.search_end_date = [$scope.currentYear, ($scope.currentMonth + 1), endDay].join('-');
-    };
-
-    $scope.setStartEndDates();
-
-    $scope.selectedYearMonthsData = function() {
-      return $scope.allYearsStats()[$scope.currentYear];
-    };
-
-    $scope.updateCurrentYearAndMonthsData = function() {
-      $scope.setStartEndDates();
-      $scope.monthsData = $scope.selectedYearMonthsData();
-      $scope.determineTheMonthToShow();
-      $scope.fetchBills();
-    };
-
-    $scope.determineTheMonthToShow = function() {
-       // select the months that has bills
-       var data = _.filter($scope.monthsData, function(m) { return m.billCount > 0; });
-
-       if (data.length > 0) {
-         var preSelectedMonth;
-         // If the selected year is the current year then pre-select the current month
-         if ($scope.currentYear == moment().year()) {
-           preSelectedMonth = moment().month();
-         } else {
-           // Pre-select the month based on the available data for that particular year
-           preSelectedMonth = moment().month(data[0].monthName).month();
-         }
-
-         $scope.updateCurrentMonth(preSelectedMonth, false);
-       }
-    };
-
-    $scope.updateCurrentMonth = function(month, disabled) {
-      if(disabled) {} else {
-        $scope.currentMonth = month;
-        $scope.setStartEndDates();
-        $scope.currentMonthName = monthNames[month];
-        $scope.fetchBills();
-      }
-    };
-
-    $scope.allYearsStats = function() {
-      var data = {};
-
-      _.each($scope.getStatsYears(), function(year) {
-        data[year] = [];
-        _.each(moment.monthsShort(), function(m) { data[year].push({monthName: m, billCount: 0}); });
+      _.each(yearStat.month_stats, function(monthStat) {
+        var month = parseInt(monthStat.month) - 1;
+        data[year][month].billCount = parseInt(monthStat.bill_count);
       });
 
-      _.each($scope.stats.year_stats, function(yearStat) {
-        var year = parseInt(yearStat.year);
+    });
 
-        _.each(yearStat.month_stats, function(monthStat) {
-          var month = parseInt(monthStat.month) - 1;
-          data[year][month].billCount = parseInt(monthStat.bill_count);
-        });
+    return data;
+  };
 
-      });
+  $scope.getStatsYears = function() {
+    var years = [];
 
-      return data;
-    };
+    _.each($scope.stats.year_stats, function(yearStat) {
+      years.push(parseInt(yearStat.year));
+    });
 
-    $scope.getStatsYears = function() {
-      var years = [];
+    // Add current year by default
+    years.push(moment().year());
 
-      _.each($scope.stats.year_stats, function(yearStat) {
-        years.push(parseInt(yearStat.year));
-      });
+    years = _.uniq(years);
 
-      // Add current year by default
-      years.push(moment().year());
+    var minYear = _.min(years);
+    var maxYear = _.max(years);
+    var ret = [minYear];
 
-      years = _.uniq(years);
+    var diff = moment([maxYear,0]).diff(minYear + '-01-01', 'years');
 
-      var minYear = _.min(years);
-      var maxYear = _.max(years);
-      var ret = [minYear];
+    _.times(diff, function(i) {
+      ret.push(minYear + (i + 1));
+    });
 
-      var diff = moment([maxYear,0]).diff(minYear + '-01-01', 'years');
+    return ret;
+  };
 
-      _.times(diff, function(i) {
-        ret.push(minYear + (i + 1));
-      });
+  // Read getParams and update the currentYear and currentMonth
+  var queryParams = SBUtils.getQueryParams(window.location.search);
+  $scope.currentYear = parseInt(queryParams.year) || $scope.currentYear;
+  $scope.currentMonth = (parseInt(queryParams.month) - 1) || $scope.currentMonth;
 
-      return ret;
-    };
-
-    // Read getParams and update the currentYear and currentMonth
-    var queryParams = SBUtils.getQueryParams(window.location.search);
-    $scope.currentYear = parseInt(queryParams.year) || $scope.currentYear;
-    $scope.currentMonth = (parseInt(queryParams.month) - 1) || $scope.currentMonth;
-
-    // Invoke init methods manually for the first time
-    // $scope.fetchBills();
-    $scope.updateCurrentYearAndMonthsData();
+  // Invoke init methods manually for the first time
+  // $scope.fetchBills();
+  $scope.updateCurrentYearAndMonthsData();
 });
 
 simpleBills.controller("AddBillController", function($scope) {
-    $scope.accountId = PageConfig ? PageConfig.accountId : "";
+  $scope.accountId = PageConfig ? PageConfig.accountId : "";
 
-    $scope.formValidator = new CustomFormValidator({
-      requiredFields: ['bill_desc', 'bill_amount']
-    });
+  $scope.formValidator = new CustomFormValidator({
+    requiredFields: ['bill_desc', 'bill_amount']
+  });
 });
